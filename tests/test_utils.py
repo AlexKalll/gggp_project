d5417dae8c4270453165df37f0ed78773c899c80
AlexKalll <alexkalalw@gmail.com>
2025-12-17 18:04:15 +0300

    test: Add utils tester

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,14 @@
-# Unit tests for utils module
+
+import unittest
+from utils import calculate_complexity, parse_program_string
+
+class TestUtils(unittest.TestCase):
+    def test_calculate_complexity(self):
+        self.assertEqual(calculate_complexity('A AND B'), 1)
+    
+    def test_parse_program_string(self):
+        result = parse_program_string('A OR B')
+        self.assertEqual(result, ['A', 'OR', 'B'])
+
+if __name__ == '__main__':
+    unittest.main()
